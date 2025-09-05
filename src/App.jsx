7b2d2e93ada5205241c06bcdf59fd2867e39b7d3
nikshay balki <balkinikshay@gmail.com>
2025-09-05 13:07:00 +0530

    complete the background effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -8,20 +9,25 @@ import Page2 from './pages/Page2'
 import Page3 from './pages/Page3'
 import Page4 from './pages/Page4'
 
+import ParticlesBackground from './components/ParticlesBackground'
+
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-black text-white">
-      <Header />
-      <main className="pt-20">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/page3" element={<Page3 />} />
-          <Route path="/page4" element={<Page4 />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <div className="relative min-h-screen bg-black text-white">
+  <ParticlesBackground />   {/* ✅ must be here */}
+  <Header />
+  <main className="relative z-10">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/page1" element={<Page1 />} />
+      <Route path="/page2" element={<Page2 />} />
+      <Route path="/page3" element={<Page3 />} />
+      <Route path="/page4" element={<Page4 />} />
+    </Routes>
+  </main>
+  <Footer />
+</div>
+
   )
 }
